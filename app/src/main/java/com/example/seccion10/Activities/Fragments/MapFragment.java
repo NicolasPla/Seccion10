90432f5c0df7e7796865f3827b7d910c7ab608e0
Nicolas <Nicolas.Pla@corbisstudio.com>
2019-02-08 11:14:59 -0300

    Geolocation completed

--- app/src/main/java/com/example/seccion10/Activities/Fragments/MapFragment.java
+++ app/src/main/java/com/example/seccion10/Activities/Fragments/MapFragment.java
@@ -3,11 +3,15 @@ package com.example.seccion10.Activities.Fragments;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
@@ -28,6 +32,7 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -36,7 +41,9 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
+import static android.content.Context.LOCATION_SERVICE;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener, LocationListener {
 
 
     private View rootView;
@@ -44,6 +51,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
     private MapView mapView;
 
     private FloatingActionButton fab;
+
+    private LocationManager locationManager;
+    private Location currentLocation;
+
+    private Marker marker;
+    private CameraPosition camera;
 
     public MapFragment() {
     }
@@ -81,6 +94,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
+
+        // Se debe castear
+        locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
+
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -91,16 +108,23 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        gMap.setMyLocationEnabled(true);
+
+
+        //gMap.setMyLocationEnabled(true);
+        //gMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 10, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 0, this);
+
     }
 
-    private boolean isGpsIsEnabled(){
+    private boolean isGpsIsEnabled() {
         try {
             int gpsSignal = Settings.Secure.getInt(getActivity().getContentResolver(), Settings.Secure.LOCATION_MODE);
 
             if (gpsSignal == 0) {
                 return false;
-            } else{
+            } else {
                 return true;
             }
         } catch (Settings.SettingNotFoundException e) {
@@ -109,7 +133,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
         }
     }
 
-    private void showAlertInfo(){
+    private void showAlertInfo() {
         new AlertDialog.Builder(getContext())
                 .setTitle("GPS Signal")
                 .setMessage("You don´t have GPS signal enabled. " +
@@ -122,16 +146,77 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, View.On
                         startActivity(intent);
                     }
                 })
-                .setNegativeButton("CANCEL",null)
+                .setNegativeButton("CANCEL", null)
                 .show();
-
     }
-
 
     @Override
     public void onClick(View v) {
-        if (!this.isGpsIsEnabled()){
+        if (!this.isGpsIsEnabled()) {
             showAlertInfo();
+        } else {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location == null){
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+            currentLocation = location;
+
+            if (currentLocation != null){
+                createOrUpdateMarkerByLocation(location);
+                returnToLocation(location);
+            }
         }
     }
+
+    private void createOrUpdateMarkerByLocation(Location location){
+        if (marker == null){
+            marker = gMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).draggable(true ));
+        } else {
+            marker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+        }
+    }
+
+    private void returnToLocation(Location location){
+        camera = new CameraPosition.Builder()
+                .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                .zoom(15)
+                .bearing(0)
+                .tilt(30)
+                .build();
+        gMap.animateCamera(CameraUpdateFactory.newCameraPosition(camera));
+
+
+    }
+
+    // Metodos que se ejecutan con GPS_PROVIDER
+
+           @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(getContext(), "Changed -> " + location.getProvider(), Toast.LENGTH_SHORT).show();
+                createOrUpdateMarkerByLocation(location);
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
 }
